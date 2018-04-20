0c9aec978f55f6a6fce71bc9a672431a474ca7e3
mobileClumsy <clumsiestide@hotmail.com>
2018-04-21 01:04:23 +0800

    ES6函数式编程练习

--- test3.js
+++ test3.js
@@ -10,3 +10,45 @@ var y = 0;
 foo( obj, y );
 console.log( obj.a, y );
 console.log( void obj.a );
+
+var func = () => console.log("Simply Function !");
+func();
+
+var identity = (v) => v;
+console.log(identity([1,2,3]));
+
+var tellType = (arg) => {
+    if ( typeof arg === "function" ) {
+        console.log( "The passed data is function !" );
+        arg()
+    }
+    else
+        console.log( "The passed data is: " + arg )
+}
+let data =1;
+tellType( data );
+
+let fn = () => {};
+tellType( fn );
+tellType( 'Hello World! ' );
+tellType( true );
+tellType( NaN );
+tellType( undefined );
+tellType( 34839.99 );
+tellType( data === 1 );
+
+// Function => String
+let crazy = () => { return String };
+console.log( crazy()( "HOC" ) );
+
+const forEachObject = (obj, fn) => {
+    for ( var property in obj) {
+        if ( obj.hasOwnProperty(property) ) {
+            fn( property, obj[ property ])
+        }
+    } 
+}
+let object = { a:1, b:2 };
+forEachObject( object, ( k, v )  => console.log( k + ":" + v ) );
+// let fn2 = ( k, v )  => console.log( k + ":" + v );
+// forEachObject( object, fn2 );
